e7993732988da7fedc242a93b24174fbb5e40d4c
jeril121 <58134635+jeril121@users.noreply.github.com>
2021-07-26 13:53:53 +0530

    Add files via upload

---
+++ src/main/java/com/example/testappsquareoff/User.kt
@@ -0,0 +1,3 @@
+package com.example.testappsquareoff
+
+data class User(var name : String ?= null,var slug : String ?= null,var year : String ?= null,var dash : String ?= null,var img : String ?= null)
